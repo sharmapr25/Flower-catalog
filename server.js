8a3e4b0f0c55c79b10a7bb6e0962e989c9699761
piolAtif <spreeti@thoughtworks.com>
2016-12-14 16:02:07 +0530

    Modified server to getting a.txt file and home page on browser

--- server.js
+++ server.js
@@ -1,34 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 
-var getContent = function(req){
-	if(req.url == '/')
-		return 'hello';
-	return fs.readFileSync('.'+req.url);
-}
-
-var callGetMethod = function(req, res){
-	res.end(getContent(req));
-};
-
-var callPostMethod = function(req, res){
-	var result = '';
-	req.on('data', function(chunk){
-		result += chunk;
-	})
-
-	req.on('end',function(){
-		res.end('hello'+result);
-	})
+var read = function(file){
+	return fs.readFileSync('.'+file);
 }
 
 var server = http.createServer(function(req, res){
-	if(req.method == 'GET'){
-		callGetMethod(req,res);
+	res.setHeader('Content-Type','text/plain');
+	console.log(req.url);
+	if(req.url == '/')
+		res.end('hello');
+	else if(req.url == '/a.txt'){
+		res.end(read(req.url));
 	}
-	callPostMethod(req, res);
-	
-
+	res.statusCode = 404;
+	res.end('jina hai toh command+q mar');
 });
 
 
