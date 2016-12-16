d9cc82ef02c9245eb1de21e379f4d4ca2bb0f839
piolAtif <spreeti@thoughtworks.com>
2016-12-16 11:57:14 +0530

    Extract server callback to controller

--- server.js
+++ server.js
@@ -1,66 +1,6 @@
 var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var comment = [];
+var controller = require('./controller');
 
-var read = function(file){
-	return fs.readFileSync(file,'utf8');
-}
-
-
-var type = {'.html':'text/html',
-	'.css':'text/css',
-	'.jpg':'image/jpg',
-	'.gif':'image/gif',
-	'.js':'application/js',
-	'.pdf':'application/pdf',
-	'.txt':'text/plain'
-	};	// '.ico':'image/ico'};
-
-var server = http.createServer(function(req, res){
-	if(req.url =='/updated'){
-		var data = '';
-		req.on('data',function(chunk){
-			data += chunk;
-		})	
-		req.on('end',function(){
-			comment.unshift(JSON.parse(data));
-			res.end(data);
-		})
-	}
-
-	if(req.url == '/previous'){
-		var data = '';	
-		res.end(JSON.stringify(comment));
-	};
-
-	if(req.url == '/contentOfFlower'){
-		var content = read('./public/text/content.txt');
-		res.end(content);
-	}
-
-	var filePath = '.'+req.url;
-
-	if(filePath == './'){
-		filePath = './public/index.html'
-	}
-	fs.readFile(filePath, function(error, content){
-		console.log('filePath',filePath);
-		if(error){
-			res.statusCode = 404;
-			filePath = './';
-			res.end('File not found');
-			
-		}
-		else{
-			var contentType = type[path.extname(filePath)];
-			res.setHeader('content-type',contentType);
-			res.end(content,'utf8');
-		}
-		
-	});		
-});
-
-
+var server = http.createServer(controller);
 
 server.listen(8080);
