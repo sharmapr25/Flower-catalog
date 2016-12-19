9141af2de12fb420abcb1b7954ef26ddc798b80d
piolAtif <spreeti@thoughtworks.com>
2016-12-19 09:25:13 +0530

    Added an object of urls for flower content and getContent

--- controller.js
+++ controller.js
@@ -1,14 +1,69 @@
+var fs = require('fs');
+var path = require('path');
 
-var controller = function(req,res){
-	if(req.url.length > 0){
-		res.statusCode = 404;
-		res.end('File not found');
-	}
-	else{
-		res.statusCode = 200;
-		res.end('welcome');
-	}
-	
+var type = {'.html':'text/html',
+	'.css':'text/css',
+	'.jpg':'image/jpg',
+	'.gif':'image/gif',
+	'.js':'application/js',
+	'.pdf':'application/pdf',
+	'.txt':'text/plain'
+	};	// '.ico':'image/ico'};
+
+var read = function(file){
+	return fs.readFileSync(file,'utf8');
 }
+
+var comment = [];
+
+var getComment = function(){
+	return JSON.stringify(comment);
+};
+
+var getContent = function(){
+	return read('./public/text/content.txt');
+}
+
+var urls = {'/previous': getComment,
+	'/contentOfFlower':getContent}
+
+var controller = function(req, res){
+	console.log('let see path ',path.isAbsolute(req.url));
+	if(req.url =='/updated'){
+		var data = '';
+		req.on('data',function(chunk){
+			data += chunk;
+		})	
+		req.on('end',function(){
+			comment.unshift(JSON.parse(data));
+			res.end(data);
+		})
+	}
+	if(urls[req.url]){
+		res.end(urls[req.url]());
+	};
+
+	var filePath = '.'+req.url;
+
+	if(filePath == './'){
+		filePath = './public/index.html'
+	}
+	fs.readFile(filePath, function(error, content){
+		console.log('filePath',filePath);
+		if(error){
+			res.statusCode = 404;
+			filePath = './';
+			res.end('File not found');
+			
+		}
+		else{
+			var contentType = type[path.extname(filePath)];
+			res.setHeader('content-type',contentType);
+			res.end(content,'utf8');
+		}
+		
+	});		
+}
+
 
 module.exports = controller;
